d126b21435704954635219477fbfb2b48c9c0393
LiXizhi <lixizhi@yeah.net>
2015-02-04 10:49:47 +0800

    config and audio files

---
+++ res/make_link.bat
@@ -0,0 +1,4 @@
+pushd %~dp0
+mklink main_full.pkg D:\lxzsrc\ParaEngine\ParaWorld\installer\main_full_mobile.pkg
+mklink /D Texture\blocks D:\lxzsrc\ParaEngine\ParaWorld\Texture\blocks
+popd
---
+++ res/remove_link.bat
@@ -0,0 +1,4 @@
+pushd %~dp0
+rm main_full.pkg
+rmdir Texture\blocks
+popd
