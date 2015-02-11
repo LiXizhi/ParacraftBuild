34f5e351332b376aa3ee852b455ee2611c38593d
LiXizhi <lixizhi@yeah.net>
2015-02-11 17:15:44 +0800

    removed unused sampleworld

--- res/worlds/DesignHouse/SampleWorld/script/36_36.onload.lua
+++
@@ -1,9 +0,0 @@
-local sceneLoader = ParaScene.GetObject("<managed_loader>36_36.onload.lua");
-if (sceneLoader:IsValid() == true) then 
-	ParaScene.Attach(sceneLoader);
-	return
-end
-sceneLoader = ParaScene.CreateManagedLoader("36_36.onload.lua");
-local player, asset, playerChar,att;
-local cpmesh=ParaScene.CreateMeshPhysicsObject;
-ParaScene.Attach(sceneLoader);
--- res/worlds/DesignHouse/SampleWorld/script/38_35.onload.lua
+++
@@ -1,9 +0,0 @@
-local sceneLoader = ParaScene.GetObject("<managed_loader>38_35.onload.lua");
-if (sceneLoader:IsValid() == true) then 
-	ParaScene.Attach(sceneLoader);
-	return
-end
-sceneLoader = ParaScene.CreateManagedLoader("38_35.onload.lua");
-local player, asset, playerChar,att;
-local cpmesh=ParaScene.CreateMeshPhysicsObject;
-ParaScene.Attach(sceneLoader);
