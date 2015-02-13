cf6b8954717ba6de1206bffc5a76b4a8a748df0f
LiXizhi <lixizhi@yeah.net>
2015-02-13 16:51:29 +0800

    added resource file

--- res/make_link.bat
+++ res/make_link.bat
@@ -1,4 +1,5 @@
 pushd %~dp0
 mklink main_full.pkg D:\lxzsrc\ParaEngine\ParaWorld\installer\main_full_mobile.pkg
+mklink main_mobile_res.pkg D:\lxzsrc\ParaEngine\ParaWorld\installer\main_mobile_res.pkg
 mklink /D Texture\blocks D:\lxzsrc\ParaEngine\ParaWorld\Texture\blocks
 popd
